166b10965bb91119d46554b5fd01fad2e15915e2
GusCorreiaSilva <gustaaavox@gmail.com>
2024-09-23 22:19:41 -0300

    first commit

---
+++ class-one.js
@@ -0,0 +1,85 @@
+// var nome = 'gustavo'
+// let temp = 'temporario'
+// const constante = 'constante'
+
+
+// console.log('hellow');
+// console.log(nome);
+// console.log(temp);
+// console.log(constante);
+
+
+// console.error()
+// console.info()
+// console.table([
+//])
+//console.warn()
+
+
+// let name = 'Github'
+// const age = '40'
+// let isAproved = false
+// let date = null
+// let address 
+// const languages = ['java', 'javascript', 'php']
+// const number = [1, 2, 3]
+
+
+// const user = {
+//     email: 'taste@teste.com',
+//     password: 'teste123',
+//     age: 10
+// }
+
+
+
+
+// number[3] = 'chocolate'
+
+// console.log(number[3])
+
+// console.log(name);
+// console.log(age);
+// console.log(isAproved);
+// console.log(date);
+// console.log(address);
+// console.log(languages);
+// console.log(user);
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAproved);
+// console.log(typeof date);
+// console.log(typeof address);
+// console.log(typeof language);
+// console.log(typeof user);
+
+//metodo string
+
+const fullName = 'Suporte de Domingo'
+
+const contaLetras = fullName.length
+
+const stringToArray = fullName.split(' ')
+
+// console.log(stringToArray);
+
+const username = 'Ola, ' + stringToArray[0] + ' ' + stringToArray[2]
+// console.log(username);
+
+//UpperCase
+const maiusculo = fullName.toUpperCase()
+
+
+//LowerCase
+const minusculo = fullName.toLowerCase()
+
+console.log(maiusculo);
+console.log(minusculo);
+
+//busca o indice
+console.log(fullName.indexOf('de'), 'indexOf');
+
+//corta string por indice
+console.log(fullName.slice(8, 10));
+
