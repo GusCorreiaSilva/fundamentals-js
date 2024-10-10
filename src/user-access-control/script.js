7ab080f83a277cd1b9bea0bb6526babf0f4a6fc6
GusCorreiaSilva <gustaaavox@gmail.com>
2024-10-09 21:51:21 -0300

    feat: User access control

---
+++ src/user-access-control/script.js
@@ -0,0 +1,26 @@
+function verificarAcesso(perfil) {
+
+    if (perfil === "administrador") {
+
+        return console.log("Bem vindo(a), administrador");
+
+    } else if (perfil === "moderador") {
+        
+        return console.log("Bem vindo(a) moderador, você pode gerenciar o conteúdo");
+        
+    }
+
+    switch (perfil) {
+        
+        case "assinante": return console.log("Ola assinante! Aproveite o conteúdo disponivel");
+        
+        default : let noAccess = perfil === "" ? console.log("Perfil não especificado") : console.log("Acesso negado");  
+    }
+
+}
+
+verificarAcesso("administrador");
+verificarAcesso("moderador");
+verificarAcesso("assinante");
+verificarAcesso("");
+verificarAcesso("visitante");
