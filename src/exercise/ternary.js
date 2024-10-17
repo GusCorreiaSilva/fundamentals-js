e1fd0734ab21dd8514218c430797b56d4d4a344d
GusCorreiaSilva <gustaaavox@gmail.com>
2024-10-16 21:36:35 -0300

    feat: ternary exercise

---
+++ src/exercise/ternary.js
@@ -0,0 +1,7 @@
+const age = 12
+
+age === 18 ? console.log('Você é de maior') : console.log('Você é de menor')
+
+age >= 18 && console.log('Pode acessar')
+age <= 17 && console.log('Você não pode acessar')
+
