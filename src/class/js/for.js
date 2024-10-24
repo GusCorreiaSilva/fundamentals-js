0a9594117c984ee93ebd1d4a2820aac415eadbbe
GusCorreiaSilva <gustaaavox@gmail.com>
2024-10-23 21:03:41 -0300

    feat: for loop

---
+++ src/class/js/for.js
@@ -0,0 +1,33 @@
+//loop
+
+// for (i = 0; i <= 5; i ++) {
+   
+//     console.log(i);
+
+// }
+
+// //Operador de adição
+// let int = 1
+//incremento pós
+// console.log(int++, 'pós');
+//incremento pré
+// console.log(++int, 'pré');
+
+//Operador de decremento
+// let dec = 10
+// //decremento pós
+// console.log(dec--, 'pós');
+//incremento pré
+// console.log(--dec, 'pré');
+
+// for (i = 1; i <= 20; i++) {
+//     // console.log('chocolate');
+    
+//     if (i % 2 === 0) {  
+//         console.log(i,'é par!');
+        
+//     } else {
+//         console.log(i,'é impar!');
+        
+//     }
+// }
