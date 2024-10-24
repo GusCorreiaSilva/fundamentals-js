9d826ef85687dacb71877a3c8400a0cc4b2d142f
Gustavo <gustaaavox@gmail.com>
2024-10-24 20:24:00 -0300

    feat:exercises 3-7 done

--- src/class/js/for-exercise.js
+++ src/class/js/for-exercise.js
@@ -1,63 +1,70 @@
 //Exercício 1: Imprimir números de 1 a 10
 // Crie um loop que imprima os números de 1 a 10.
 
-
-for (i = 0; i <= 10; i++ ) {
-    console.log(i);
-    
-}
+// for (i = 0; i <= 10; i++ ) {
+//     console.log(i);
+// }
 
 // Exercício 2: Imprimir números pares entre 1 e 20
 // Crie um loop que imprima apenas os números pares de 1 a 20.
 
-for (i = 0; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(i, 'é par');
+// for (i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i, 'é par');
         
-    }
-    
-}
+//     } 
+// }
 
 // Exercício 3: Somar os números de 1 a 100
 // Crie um loop que calcule a soma de
 //  todos os números de 1 a 100.
 
-for (i = 0; i <= 100; ++i) {
-    console.log(i);
-    
-}
+// let soma = 0
+// for (let numbers = 1; numbers <= 100; numbers++) {
+//     soma += numbers
+// }
+// console.log("a soma dos números é",soma);  
 
 // Exercício 4: Imprimir os elementos de um array
 // Dado um array de frutas, use um loop para imprimir cada fruta.
+const fruits = ["Apples", "Pineapple", "Orange", "Dragon fruit", "Jamelão"]
 
-// -----------------------------
+for(let fruit of fruits) {
+    console.log(fruit);
+}
 
 // Exercício 5: Multiplicar os números de 1 a 10 por 2
 // Crie um loop que imprima o resultado da multiplicação de cada número de 1 a 10 por 2.
 
-// -----------------------------
+soma = 0
+for(let numbah = 1; numbah <=10; numbah += 2) {
+    soma += numbah
+}
+console.log("a soma dos números é",soma);
 
 // Exercício 6: Imprimir números de 10 a 1 (contagem regressiva)
 // Crie um loop que imprima os números de 10 até 1.
 
-// -----------------------------
+for ( i = 10; i >= 1; i--) {
+    console.log(i);
+}
 
 // Exercício 7: Encontrar o maior número em um array
 // Dado um array de números, use um loop para encontrar e imprimir o maior número.
 
-// -----------------------------
+const numList = [5,2,1,6,7,0,56]
 
 // Exercício 8: Contar números ímpares entre 1 e 50
 // Crie um loop que conte quantos números ímpares existem entre 1 e 50 e imprima o resultado.
 
-// -----------------------------
+
 
 // Exercício 9: Imprimir a tabuada de um número
 // Dado um número, use um loop para imprimir a tabuada desse número (de 1 a 10).
 
-// -----------------------------
+
 
 // Exercício 10: Reverter uma string
 // Dada uma string, use um loop para imprimi-la de trás para frente.
 
-// -----------------------------
+
