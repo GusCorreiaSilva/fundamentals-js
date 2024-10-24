072b110b30d46cf1464012206f3cccbd77d39ab2
GusCorreiaSilva <gustaaavox@gmail.com>
2024-10-23 22:14:07 -0300

    feat: for-exercise

---
+++ src/class/js/for-exercise.js
@@ -0,0 +1,63 @@
+//Exercício 1: Imprimir números de 1 a 10
+// Crie um loop que imprima os números de 1 a 10.
+
+
+for (i = 0; i <= 10; i++ ) {
+    console.log(i);
+    
+}
+
+// Exercício 2: Imprimir números pares entre 1 e 20
+// Crie um loop que imprima apenas os números pares de 1 a 20.
+
+for (i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i, 'é par');
+        
+    }
+    
+}
+
+// Exercício 3: Somar os números de 1 a 100
+// Crie um loop que calcule a soma de
+//  todos os números de 1 a 100.
+
+for (i = 0; i <= 100; ++i) {
+    console.log(i);
+    
+}
+
+// Exercício 4: Imprimir os elementos de um array
+// Dado um array de frutas, use um loop para imprimir cada fruta.
+
+// -----------------------------
+
+// Exercício 5: Multiplicar os números de 1 a 10 por 2
+// Crie um loop que imprima o resultado da multiplicação de cada número de 1 a 10 por 2.
+
+// -----------------------------
+
+// Exercício 6: Imprimir números de 10 a 1 (contagem regressiva)
+// Crie um loop que imprima os números de 10 até 1.
+
+// -----------------------------
+
+// Exercício 7: Encontrar o maior número em um array
+// Dado um array de números, use um loop para encontrar e imprimir o maior número.
+
+// -----------------------------
+
+// Exercício 8: Contar números ímpares entre 1 e 50
+// Crie um loop que conte quantos números ímpares existem entre 1 e 50 e imprima o resultado.
+
+// -----------------------------
+
+// Exercício 9: Imprimir a tabuada de um número
+// Dado um número, use um loop para imprimir a tabuada desse número (de 1 a 10).
+
+// -----------------------------
+
+// Exercício 10: Reverter uma string
+// Dada uma string, use um loop para imprimi-la de trás para frente.
+
+// -----------------------------
