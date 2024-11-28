4e38e739add12b8bb9fe20e3c44d07955adc8e08
GusCorreiaSilva <gustaaavox@gmail.com>
2024-11-27 22:24:58 -0300

    feat: revisão

--- src/class-four/fun.js
+++ src/class-four/fun.js
@@ -1,5 +1,5 @@
 // Metodo length, conta caracteres.
-const fullName = 'Lucas do Espirito Santo'
+// const fullName = 'Lucas do Espirito Santo'
 // console.log(fullName.length, 'conta caracteres');
 
 // const fruits = ['maçã', 'pera', 'uva', 'pessego']
@@ -14,8 +14,33 @@ const fullName = 'Lucas do Espirito Santo'
 // console.log(cortaNome);
 
 //split corta transformando em array
-const transformToArray = fullName.split(' ')
+// const transformToArray = fullName.split(' ')
 // console.log(transformToArray);
 
-const lastName = transformToArray[transformToArray.length -1]
-console.log(lastName);
+// const lastName = transformToArray[transformToArray.length -1]
+// console.log(lastName);
+
+//função que pegue o primeiro nome e da ola
+
+// function FirstName(array) {
+//     if(array.length <= 0) {
+//         return console.error(`Coloque um nome valido`)
+//     }
+//     return `Olá ${array.split(' ')[0]}, seja bem vindo`
+// }
+// console.log(FirstName(''));
+
+function CalcularImc(peso, altura) {
+      
+     let imc = peso / (altura * altura)
+     if (imc < 18.5) {
+        console.log('Abaixo do peso normal');
+         
+     }  else if(imc >= 18.5 && imc <= 24.99) {
+        console.log('Peso normal');
+        
+     } else if ( imc > 25 && imc <= 29.99){
+        console.log('Acima do peso');
+     }
+}
+console.log(CalcularImc(59, 1.78).toFixed(1));
